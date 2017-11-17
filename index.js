d53aa851cdb87257fa420c771f075a7f12777a3f
aelkanabi <adil.kanabi@gmail.com>
2017-11-17 18:09:16 +0100

    testing material design

--- index.js
+++ index.js
@@ -1,65 +1,33 @@
-var config = require('./config');
-var logs = require('./logs');
+const config = require('./config');
+const log = require('./logs');
 
 //loading express
-logs.log("Loading express....");
-try {
-    var express = require('express');
-    var app = express();
-    logs.success('OK\n');
-} catch (exception) {
-    logs.error('NOK\n');
-    process.exit(1);
-}
+const express = require('express');
+const app = express();
 
 //loading http server
-logs.log("Loading "+ config.PROTOCOL +"server....");
-try {
-    var server = require(config.PROTOCOL).createServer(app);
-    logs.success('OK\n');
-} catch (exception) {
-    logs.error('NOK\n');
-    process.exit(1);
-}
+const server = require(config.PROTOCOL).createServer(app);
 
 
 //Using ejs as a templating engine
-logs.log('Loading ejs templating engine....');
-try {
-    var engine = require('ejs-locals');
-    var path = require("path");
-    app.set('views', path.join(__dirname, 'views'));
-    app.engine('ejs', engine);
-    app.set('view engine', 'ejs');
-    logs.success('OK\n');
-} catch (exception) {
-    logs.error('NOK : ' + exception.message +  '\n');
-    process.exit(1);
-}
+const engine = require('ejs-locals');
+const path = require("path");
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
 
 //Loading routes
-logs.log('Loading routes....');
-try {
-    var routes = require('./routes')(app);
-    logs.success('OK\n');
-} catch (exception) {
-    logs.error('NOK\n');
-    process.exit(1);
-}
+require('./routes')(app);
 
 //serving static files in the public folder (css, js, images)
-logs.log('Serving static files....');
-try {
-    var statics = require('./public');
-    statics.serve(app, express);
-    logs.success('OK\n');
-} catch (exception) {
-    logs.error('NOK\n');
-    process.exit(1);
-}
+const statics = require('./public');
+statics.serve(app, express);
 
-logs.info('Listening on port '+ config.PORT +'...'+"\n");
+//Listen on port specified in the config file
 server.listen(config.PORT).on('error', function(err) {
-    logs.error('NOK\n');
-    logs.error(err + '\n');
+    log.error(err);
 });
+
+//open the webpage on startup
+const opn = require('opn');
+opn(config.PROTOCOL + '://localhost:'+config.PORT);
